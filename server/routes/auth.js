802628027a880c8040b4b1b7c36e1f936c54e3cf
Lukasz_Kochajewski <lkochajewski@pgs-soft.com>
2020-05-18 18:45:25 +0200

    update server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,15 @@ const axios = require('axios')
 const router = Router()
 
 router.post('/', (req, res, next) => {
-  const { headers, body, authorizeUrl } = req.body
+  const { body } = req.body
+  const authorizeUrl = process.env.AUTHORIZE_URL
+  const headers = {
+    'X-Auth-Client': process.env.CLIENT_ID,
+    'X-Auth-Token': process.env.ACCESS_TOKEN
+  }
 
-  axios.post(authorizeUrl, body, { headers })
+  axios
+    .post(authorizeUrl, body, { headers })
     .then((result) => {
       return res.json(result.data)
     })
--- store/core.js
+++ store/core.js
@@ -17,20 +17,20 @@ export const getters = {
 
 export const actions = {
   async authorize ({ commit, state }, payload) {
-    if (state.token) { return }
+    if (state.token) {
+      return
+    }
     const body = {
       channel_id: 1,
       expires_at: 1602288000,
       allowed_cors_origins: ['https://nuxt-store.netlify.app/']
     }
-    const { data } = await this.$axios.$post(`${process.env.SERVER_URL}/api/auth`, {
-      authorizeUrl: process.env.AUTHORIZE_URL,
-      body,
-      headers: {
-        'X-Auth-Client': process.env.CLIENT_ID,
-        'X-Auth-Token': process.env.ACCESS_TOKEN
+    const { data } = await this.$axios.$post(
+      `${process.env.SERVER_URL}/api/auth`,
+      {
+        body
       }
-    })
+    )
     commit('setToken', data.token)
   }
 }
